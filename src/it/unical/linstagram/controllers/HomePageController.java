b90231d48df17ad1833f3c3bcb6de7a4553cf41e
lorenzobrusco <lorenzo.brusco@libero.it>
2017-12-06 15:49:08 +0100

    first commit

---
+++ src/it/unical/linstagram/controllers/HomePageController.java
@@ -0,0 +1,15 @@
+package it.unical.linstagram.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomePageController {
+
+	
+	
+	@RequestMapping("/")
+	public String homePageController() {
+		return "index";
+	}
+}
