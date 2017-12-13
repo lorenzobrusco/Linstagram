9c6985c394b53244b9aa5eb8dc21ff86f4a0257d
lorenzobrusco <lorenzo.brusco@libero.it>
2017-12-13 17:02:15 +0100

    commit

--- src/it/unical/linstagram/tests/UserTest.java
+++ src/it/unical/linstagram/tests/UserTest.java
@@ -1,27 +1,14 @@
 package it.unical.linstagram.tests;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.util.List;
-
-import org.hibernate.Session;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import it.unical.linstagram.model.User;
-import it.unical.linstagram.persistence.HibernateUtil;
 import it.unical.linstagram.persistence.ModelDAO;
 
-class UserTest {
-
-	@Test
-	void SaveTest() {
-		User u=new User("Eliana", "elianalovehitler@gmail.com", "Leader");
-		System.out.println(ModelDAO.getInstance().save(u));
-		assertEquals(1, ModelDAO.getInstance().getAll(User.class).size());
-	}
+public class UserTest {
 	
 	@Test
-	void followerTest() {
+	public void followerTest() {
 		User u=new User("Eliana", "elianalovehitler@gmail.com", "Leader");
 		User u1=new User("A","A","A");
 		User u2=new User("B","B","B");
@@ -30,20 +17,10 @@ class UserTest {
 		ModelDAO.getInstance().save(u1);
 		ModelDAO.getInstance().save(u2);
 		
-		u.getFollowers().add(u1);
-		u.getFollowers().add(u2);
+		u.getFollowings().add(u1);
+		u.getFollowings().add(u2);
 		
 		ModelDAO.getInstance().update(u);
-		
-		Session sex = HibernateUtil.getHibernateSession();
-		List<User> list = sex.createNativeQuery("SELECT * FROM follower",User.class).list();
-		System.out.println("_____________________________________________________________________");
-		for (User user : list) {
-			System.out.println(user.getName());
-		}
-				
-		
-		System.out.println("_____________________________________________________________________");
 		
 	}
 
