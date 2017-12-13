aeb301694c05283ad3ed9fe3f3ee97cbca77d199
Alessio Scarfone <alessioscarfone93@gmail.com>
2017-12-13 16:27:17 +0100

    Logic Update

---
+++ src/it/unical/linstagram/helper/UserManager.java
@@ -0,0 +1,28 @@
+package it.unical.linstagram.helper;
+
+import it.unical.linstagram.model.User;
+
+public class UserManager {
+
+	/**
+	 * @param id: searched user by id  
+	 * @param me: logged user
+	 * @return
+	 */
+	public User loadUser(int id, User me) {
+		return null;
+	}
+	
+	/**
+	 * @param user: searched user  
+	 * @param me: logged user
+	 * @return
+	 */
+	public User loadUser(User user, User me) {
+		return null;
+	}
+	
+	
+	
+
+}
---
+++ src/it/unical/linstagram/persistence/ModelDAO.java
@@ -0,0 +1,62 @@
+package it.unical.linstagram.persistence;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class ModelDAO {
+
+	private static ModelDAO dao;
+
+	public static ModelDAO getInstance() {
+		if (dao == null)
+			dao = new ModelDAO();
+		return dao;
+	}
+
+	private ModelDAO() {
+
+	}
+
+	public boolean save(Object model) {
+		final Session session = HibernateUtil.getHibernateSession();
+		Transaction transaction = null;
+		try {
+			transaction = session.beginTransaction();
+			session.save(model);
+			transaction.commit();
+			return true;
+		} catch (Exception e) {
+			transaction.rollback();
+			return false;
+		} finally {
+			session.close();
+		}
+	}
+
+	public List<?> getAll(Class<?> object) {
+		final Session session = HibernateUtil.getHibernateSession();
+//		System.out.println(String.format("SELECT * FROM %s", object.getSimpleName().toLowerCase()));
+		List<?> list = session.createNativeQuery(String.format("SELECT * FROM %s", object.getSimpleName().toLowerCase()), object).list();
+		session.close();
+		return list;
+	}
+	
+	public boolean update(Object model) {
+		final Session session = HibernateUtil.getHibernateSession();
+		Transaction transaction = null;
+		try {
+			transaction = session.beginTransaction();
+			session.update(model);
+			transaction.commit();
+			return true;
+		} catch (Exception e) {
+			transaction.rollback();
+			return false;
+		} finally {
+			session.close();
+		}
+	}
+
+}
