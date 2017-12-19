ce68317e78c9b7583e76b0e9e6735d1abcd1db9a
lorenzobrusco <lorenzo.brusco@libero.it>
2017-12-19 09:35:02 +0100

    add comment

--- src/it/unical/linstagram/helper/FileModel.java
+++ src/it/unical/linstagram/helper/FileModel.java
@@ -5,36 +5,39 @@ import java.io.File;
 import org.springframework.web.multipart.MultipartFile;
 
 public class FileModel {
-   private MultipartFile file;
+	private MultipartFile file;
 
-   public MultipartFile getFile() {
-      return file;
-   }
+	public MultipartFile getFile() {
+		return file;
+	}
 
-   public void setFile(MultipartFile file) {
-      this.file = file;
-   }
-   
-   public void createFolder(String path) {
-	   
-	   File theDir = new File(path);
-	
-		// if the directory does not exist, create it
+	public void setFile(MultipartFile file) {
+		this.file = file;
+	}
+
+	/**
+	 * Create directory whether it doesn't exist
+	 * 
+	 * @param path
+	 */
+	public void createFolder(String path) {
+
+		File theDir = new File(path);
+
 		if (!theDir.exists()) {
-		    System.out.println("creating directory: " + theDir.getName());
-		    boolean result = false;
-		
-		    try{
-		        theDir.mkdir();
-		        result = true;
-		    } 
-		    catch(SecurityException se){
-		        //handle it
-		    }        
-		    if(result) {    
-		        System.out.println("DIR created");  
-		    }
+			System.out.println("creating directory: " + theDir.getName());
+			boolean result = false;
+
+			try {
+				theDir.mkdir();
+				result = true;
+			} catch (SecurityException se) {
+				// handle it
+			}
+			if (result) {
+				System.out.println("DIR created");
+			}
 		}
-   }
-   
+	}
+
 }
