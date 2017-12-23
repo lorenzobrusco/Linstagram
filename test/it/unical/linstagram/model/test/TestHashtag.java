ca5546c3b06e0c6c0f38abcad943cb72885e71c9
Paola Arcuri <arcuri.paola.93@gmail.com>
2017-12-23 18:02:34 +0100

    added superclass to tests that use hsqldb, added methods for cleaning and setting up DB. testUserTagged() and followerTest() still doesn't work

--- test/it/unical/linstagram/model/test/TestHashtag.java
+++ test/it/unical/linstagram/model/test/TestHashtag.java
@@ -12,11 +12,13 @@ import it.unical.linstagram.model.Hashtag;
 import it.unical.linstagram.model.Media;
 import it.unical.linstagram.model.Post;
 import it.unical.linstagram.model.User;
+import it.unical.linstagram.persistence.HashtagDAO;
 import it.unical.linstagram.persistence.HibernateUtil;
 import it.unical.linstagram.persistence.ModelDAO;
 
-public class TestHashtag {
+public class TestHashtag extends AbstractModelTest{
 
+	
 	@Test
 	public void testHashtag()
 	{
@@ -27,17 +29,38 @@ public class TestHashtag {
 		post.getHashtags().add(new Hashtag("bellofigo"));
 		post.getHashtags().add(new Hashtag("cicciociccio"));
 		md.save(post);
-		
+
 		final Session session = HibernateUtil.getHibernateTestSession();
-	
+
 		List<Hashtag> hashtags =  session.createQuery("SELECT post.hashtags FROM Post post WHERE post.id =:idPost")
 				.setParameter("idPost",post.getId()).list();
-		
+
 		for(Hashtag h : hashtags) {
 			System.out.println(h.getHashtag());
 		}
-		
+
 		Assert.assertEquals(2,hashtags.size());
 		session.close();
 	}
+
+	@Test
+	public void testHashtagDAO()
+	{	
+		User eliana = new User("Eliana","email","pass");
+		ModelDAO md= new ModelDAO();
+		List<Media> media = new ArrayList<>();
+		Post post = new Post(eliana,media,Calendar.getInstance(),"Sono scema");
+		post.getHashtags().add(new Hashtag("bellofigo"));
+		post.getHashtags().add(new Hashtag("cicciociccio"));
+		md.save(post);
+
+		HashtagDAO hashtagDAO = new HashtagDAO();
+		Hashtag hashtagByValue = hashtagDAO.getHashtagByValue("pippo");
+		
+		System.out.println(hashtagByValue);
+		Assert.assertNull(hashtagByValue);
+		
+		
+	}
+
 }
