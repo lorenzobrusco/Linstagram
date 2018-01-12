71f081792adea8d5693a5ed91f3a4f6489d1cc2d
nananan <eliana-c@live.it>
2018-01-12 11:47:06 +0100

    Delete class EncryptPassword and changed with encrypt password of spring

--- src/it/unical/linstagram/config/CustomPasswordEncoder.java
+++ src/it/unical/linstagram/config/CustomPasswordEncoder.java
@@ -1,23 +1,71 @@
 package it.unical.linstagram.config;
 
-import org.springframework.security.crypto.password.PasswordEncoder;
+import java.security.MessageDigest;
 
-import it.unical.linstagram.helper.EncryptPassword;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class CustomPasswordEncoder implements PasswordEncoder {
 	@Override
 
 	public String encode(CharSequence rawPassword) {
-		String hashed = EncryptPassword.encrypt(rawPassword.toString()); 
+		String hashed = encrypt(rawPassword.toString()); 
 		return hashed;
 	}
 
 	@Override
 	public boolean matches(CharSequence rawPassword, String encodedPassword) {
-		return EncryptPassword.checkPassword(rawPassword.toString(), encodedPassword) != null;
+		return checkPassword(rawPassword.toString(), encodedPassword) != null;
 
 	}
 
+	private String encrypt(String password) {
+		String hash = null;
+		
+		try {
+			hash = byteArrayToHexString(computeHash(password));
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return hash;
+	}
+	
+	private String checkPassword(String newPassword, String savedPassword) {
+		String hash;
+		try {
+			hash = byteArrayToHexString(computeHash(newPassword));
+			if (hash.equals(savedPassword)) 
+				return hash;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+	private byte[] computeHash(String x) throws Exception {
+		MessageDigest d =null;
+	    d = MessageDigest.getInstance("SHA-1");
+	    d.reset();
+	    d.update(x.getBytes());
+	    return  d.digest();
+	}
+
+	private String byteArrayToHexString(byte[] b) {
+		 
+		StringBuffer sb = new StringBuffer(b.length * 2);
+	    for (int i = 0; i < b.length; i++) {
+	    	int v = b[i] & 0xff;
+	    	if (v < 16)
+	    		sb.append('0');
+	    	 
+	    	sb.append(Integer.toHexString(v));
+	    }
+	    return sb.toString().toUpperCase();
+	}
+	
 
 
 }
