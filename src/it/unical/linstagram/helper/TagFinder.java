8382c6b5831d60ff3b46db68f2906742f4912e5c
Alessio Scarfone <alessioscarfone93@gmail.com>
2018-01-15 16:19:11 +0100

    hash and tag parser

--- src/it/unical/linstagram/helper/TagFinder.java
+++ src/it/unical/linstagram/helper/TagFinder.java
@@ -1,15 +1,13 @@
 package it.unical.linstagram.helper;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class TagFinder {
 
 	public static Set<String> findTags (String text)
 	{
-		String[] arr = text.split(" ");    
+		String[] arr = text.split("\\s");    
 		Set<String> tags = new HashSet<>();
 
 		for ( String ss : arr) {
